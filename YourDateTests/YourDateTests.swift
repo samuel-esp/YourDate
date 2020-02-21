37acb9f645bcd9b1b44b1ddc4d812af96341e8e5
Samuel Esposito <samuel-esp@hotmail.it>
2020-02-21 14:36:36 +0100

    Initial Commit

---
+++ YourDateTests/YourDateTests.swift
@@ -0,0 +1,34 @@
+//
+//  YourDateTests.swift
+//  YourDateTests
+//
+//  Created by Samuel Esposito on /212/20.
+//  Copyright © 2020 Samuel Esposito. All rights reserved.
+//
+
+import XCTest
+@testable import YourDate
+
+class YourDateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
