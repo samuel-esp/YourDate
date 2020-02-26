6cb2e38e4a8bf561e107ccedb8a81ad54ff7a51d
Samuel Esposito <samuel-esp@hotmail.it>
2020-02-26 16:25:22 +0100

    Added Auth ViewControllers And Refactored Code

---
+++ YourDate/Controller/AuthenticationControllers/LaunchScreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LaunchScreenViewController.swift
+//  YourDate
+//
+//  Created by Samuel Esposito on /262/20.
+//  Copyright © 2020 Samuel Esposito. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        Timer.scheduledTimer(withTimeInterval: 4, repeats: false) { (timer) in
+            self.performSegue(withIdentifier: "launchScreenSegue", sender: self)
+            return
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YourDate/Controller/AuthenticationControllers/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  YourDate
+//
+//  Created by Samuel Esposito on /262/20.
+//  Copyright © 2020 Samuel Esposito. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
