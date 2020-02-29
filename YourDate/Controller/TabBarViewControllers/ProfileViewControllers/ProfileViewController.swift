6a88fa6c6aba7ef5379eb54f0f82c5b76c7cd06e
Samuel Esposito <samuel-esp@hotmail.it>
2020-02-29 20:35:52 +0100

    Designed Profile View

---
+++ YourDate/Controller/TabBarViewControllers/ProfileViewControllers/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  YourDate
+//
+//  Created by Samuel Esposito on /292/20.
+//  Copyright © 2020 Samuel Esposito. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
